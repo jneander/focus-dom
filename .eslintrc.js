1676f3e0b60ceb45611fa9b22990b898cd18367d
Jeremy Neander <jeremy.neander@gmail.com>
2023-02-18 09:14:46 -0600

    chore: rename _specs_ to specs

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
     },
 
     {
-      files: ['./**/*.spec.js', './**/_specs_/**/*.js'],
+      files: ['./**/*.spec.js', './**/specs/**/*.js'],
 
       globals: {
         expect: 'writable',
